00cb20e5f9e8fb45d8bbd977dddf8a02ae5e3270
Terence O'Donoghue <terence@creativelittledots.co.uk>
2016-07-16 13:47:34 +0200

    Major Changes

--- templates/content-recently-viewed-products.php
+++ templates/content-recently-viewed-products.php
@@ -16,36 +16,21 @@ global $woocommerce_recently_viewed_products;
 
 <?php if( have_posts() ) : ?>
 
-	<?php
-		/**
-		 * woocommerce_before_shop_loop hook
-		 *
-		 * @hooked woocommerce_result_count - 20
-		 * @hooked woocommerce_catalog_ordering - 30
-		 */
-		do_action( 'woocommerce_before_recently_viewed_products_loop' );
-	?>
+	<?php do_action( 'woocommerce_before_recently_viewed_products_loop' ); ?>
 	
-	<h2 class="<?php echo implode(' ', apply_filters('woocommerce_before_recently_viewed_products_title_classes', array('title'))); ?>"><?php echo apply_filters('woocommerce_before_recently_viewed_products_title', __('Recently viewed by you', 'woocommerce-recently-viewed-products')); ?></h2>
+	<h2 class="title"><?php echo apply_filters('woocommerce_before_recently_viewed_products_title', __('Recently viewed by you', 'woocommerce-recently-viewed-products')); ?></h2>
 	
 	<?php woocommerce_product_loop_start(); ?>
 	
 	<?php while( have_posts() ) : the_post(); ?>
 	
-		<?php wc_get_template(apply_filters('woocommerce_recently_viewed_product_template', 'content-product.php')); ?>
+		<?php wc_get_template( 'content-recently-viewed-product.php', array(), '', $woocommerce_recently_viewed_products->plugin_path() . '/templates/' ); ?>
 	
 	<?php endwhile; ?>
 	
 	<?php woocommerce_product_loop_end(); ?>
 
-	<?php
-		/**
-		 * woocommerce_after_shop_loop hook
-		 *
-		 * @hooked woocommerce_pagination - 10
-		 */
-		do_action( 'woocommerce_after_recently_viewed_products_loop' );
-	?>
+	<?php do_action( 'woocommerce_after_recently_viewed_products_loop' ); ?>
 
 <?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) && apply_filters('woocommerce_after_recently_viewed_products_display_no_products_found', true) ) : ?>
 
